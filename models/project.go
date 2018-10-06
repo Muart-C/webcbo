a3171d51cae39bd1c890db6110779d0ebf64f827
Muart-C <charles.muathe@strathmore.edu>
2018-10-06 08:05:26 +0300

    Project model fields updated

--- models/project.go
+++ models/project.go
@@ -1,9 +1,18 @@
 package models
 
+import "time"
+
 type Project struct {
 	BaseModel
 	ProjectName string `gorm:"not null"`
-	ProjectDescription interface{} `gorm:"type:varchar(255);not null"`
+	ProjectDescription string `gorm:"type:text; not null"`
 	ProjectBudget float64 `gorm:"null"`
-	ProjectLaborCost float64 `gorm`
+	ProjectLaborCost float64 `gorm:"type:decimal(10,2)"`
+	ProjectMaterialCost float64 `gorm:type:decimal(10,2)"`
+	ProjectPlannedStartDate time.Time `gorm:"type:timestamp" json:"startedOn"`
+	ProjectPlannedEndDate time.Time `gorm:"type:timestamp" json:"EndedOn"`
+	ProjectActualStartDate time.Time `gorm:"type:timestamp" json:"StartedOn"`
+	ProjectActualEndDate time.Time `gorm:"type:timestamp" json:"EndedOn"`
+
+
 }
